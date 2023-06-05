1c430265bbe39f089419204a970dedd5deb93212
zlatovvd <zlatovvd@ukr.net>
2023-06-05 22:34:24 +0300

    upd 05.06

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,9 +3,10 @@ const refs = {
   menuCloseBtn: document.querySelector(".menu__close-btn"),
   mobileMenu: document.querySelector(".mobile-menu"),
   mobileNavBtn: document.querySelector(".mobile-nav-btn"),
-  buyNowBtn: document.querySelector(".buy-now__btn"),
+  buyNowBtn: document.querySelectorAll(".buy-now__btn"),
   buyNowModal: document.querySelector(".backdrop"),
   modalCloseBtn: document.querySelector(".modal__close-btn"),
+  anchors: document.querySelectorAll('a[href*="#"]'),
 };
 
 const toggleMenu = () => {
@@ -13,12 +14,26 @@ const toggleMenu = () => {
 };
 
 const togleModal = () => {
-    console.log('click');
   refs.buyNowModal.classList.toggle("is-hidden");
 };
 
 refs.menuOpenBtn.addEventListener("click", toggleMenu);
 refs.menuCloseBtn.addEventListener("click", toggleMenu);
-refs.buyNowBtn.addEventListener("click", togleModal);
 refs.modalCloseBtn.addEventListener("click", togleModal);
 refs.mobileNavBtn.addEventListener("clicl", togleModal);
+
+for (let buyNow of refs.buyNowBtn) {
+  buyNow.addEventListener("click", togleModal);
+}
+
+for(let anchor of refs.anchors) {
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+    const blockId = anchor.getAttribute('href');
+    refs.mobileMenu.classList.remove("is-visible");
+    document.querySelector(blockId).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
